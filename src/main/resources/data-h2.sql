8b5690b3e0cd4ef8babe5ff29266138dd033d74f
desiCoda <j.pushkarsingh@gmail.com>
2018-04-15 12:48:39 +0530

    added data-h2.sql file for in memory create-drop database

---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,11 @@
+INSERT INTO category (description) VALUES ('Indian');
+INSERT INTO  category (description) VALUES ('Italian');
+INSERT INTO  category (description) VALUES ('American');
+INSERT INTO  category (description) VALUES ('Fast Food');
+INSERT INTO  unit_of_measurement (description) VALUES ('Teaspoon');
+INSERT INTO  unit_of_measurement (description) VALUES ('Cup');
+INSERT INTO  unit_of_measurement (description) VALUES ('Pinch');
+INSERT INTO  unit_of_measurement (description) VALUES ('Tablespoon');
+INSERT INTO  unit_of_measurement (description) VALUES ('Ounce');
+INSERT INTO  unit_of_measurement (description) VALUES ('Dash');
+INSERT INTO  unit_of_measurement (description) VALUES ('Each');
